4c2fe02471309b88eec53e63f54f668888544b5f
Diego Peres <diego.peres@corp.globo.com>
2015-03-27 20:52:46 -0300

    daredevil.js: Adding DaredevilCommand

--- src/daredevil.js
+++ src/daredevil.js
@@ -1,53 +1,15 @@
-function Daredevil(){
-  var speech = new webkitSpeechRecognition();
-  var that = this;
+function DaredevilCommand(obj){
+  var hasAllAttr = obj.hasOwnProperty('command') &&
+                   obj.hasOwnProperty('callback');
 
-  this.routes = [];
-  this.initialize = function(userOptions){
-    speech.continuos = true;
-    speech.interimResults = false;
-    speech.lang = 'pt-BR';
+  if (!hasAllAttr){ return ; }
 
-    speech.onresult = function(e){
-      index = e.results.length - 1;
-      text = e.results[index][0].transcript;
-      that.checkActions(text);
-      console.log(text);
-    };
+  var callbackIsFunction = typeof obj.callback === 'function';
+  var commandIsString = typeof obj.command === 'string';
 
-    speech.onend = function() {
-        this.start();
-    };
-  }
+  if (!(callbackIsFunction && commandIsString)){ return ; }
 
-  this.start = function(){
-    speech.start();
-  }
-
-  this.addActions = function(action){
-    var isActionExist = false;
-    if (action){
-      this.routes.forEach(function(element){
-        if (element['regexp'] == action['regexp']){
-          isActionExist = true;
-        }
-      });
-
-      if(!isActionExist){
-        this.routes.push(action);
-      }
-    }
-    return !isActionExist
-  }
-
-  this.checkActions = function(text){
-    this.routes.forEach(function(element){
-      var currentRegex = new RegExp(element['regexp'], 'i');
-      if(currentRegex.test(text)){
-        element['callback'](text);
-      }
-    });
-  }
-
-  this.initialize()
-}
+  eventCommand = new CustomEvent('DaredevilCommand', {'detail': obj});
+  eventCommand.initEvent("daredevilAddCommand", false, false);
+  document.dispatchEvent(eventCommand);
+};
